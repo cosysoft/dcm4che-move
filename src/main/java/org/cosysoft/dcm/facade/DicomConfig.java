aab48e5eee7822e737dbb5a1755b4e12e63a2dfa
Bluesky <remotejavasky@163.com>
2014-04-07 22:09:17 +0800

    rename tool name

---
+++ src/main/java/org/cosysoft/dcm/facade/DicomConfig.java
@@ -0,0 +1,46 @@
+package org.cosysoft.dcm.facade;
+
+
+public class DicomConfig {
+
+	private String remoteAEUrl;
+
+	private String localAEUrl;
+
+	private String localAETitle;
+
+	private String wadoURI;
+
+	public String getRemoteAEUrl() {
+		return remoteAEUrl;
+	}
+
+	public void setRemoteAEUrl(String remoteAEUrl) {
+		this.remoteAEUrl = remoteAEUrl;
+	}
+
+	public String getLocalAEUrl() {
+		return localAEUrl;
+	}
+
+	public void setLocalAEUrl(String localAEUrl) {
+		this.localAEUrl = localAEUrl;
+	}
+
+	public String getLocalAETitle() {
+		return localAETitle;
+	}
+
+	public void setLocalAETitle(String localAETitle) {
+		this.localAETitle = localAETitle;
+	}
+
+	public String getWadoURI() {
+		return wadoURI;
+	}
+
+	public void setWadoURI(String wadoURI) {
+		this.wadoURI = wadoURI;
+	}
+
+}
